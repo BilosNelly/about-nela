94389095f38823f898d5f9f94d6df95beda5e5e9
BilosNelly <bilosantonela1996@gmail.com>
2022-11-22 18:45:37 +0100

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,52 +1,57 @@
 import shiba from "./shiba.svg";
+import avatar from "./avatar.svg";
 import "./App.scss";
 import { Container } from "./components";
 
 function App() {
   return (
     <div className="my-web">
-
       <div className="my-web__header">
         <span>Software developer with the crazy shiba inu!</span>
         <span>Meet me, Bilos Antonela</span>
       </div>
 
-      <div className="my-web__header__img">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <div>here will be img</div>
+      <div className="my-web__content">
+        <Container
+          img={shiba}
+          title={"Meet Tokyo"}
+          text={
+            "Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky"
+          }
+        />
+
+        <Container
+          img={avatar}
+          title={"Meet Tokyo"}
+          text={
+            "Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky"
+          }
+        />
+
+        <Container
+          img={shiba}
+          title={"Meet Tokyo"}
+          text={
+            "Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky"
+          }
+        />
+
+        <Container
+          img={shiba}
+          title={"Meet Tokyo"}
+          text={
+            "Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky"
+          }
+        />
+
+        <Container
+          img={shiba}
+          title={"Meet Tokyo"}
+          text={
+            "Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky"
+          }
+        />
       </div>
-
-<div className="my-web__content">
-      <Container
-        img={shiba}
-        title={'Meet Tokyo'}
-        text={'Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky'}
-      />
-
-      <Container
-        img={shiba}
-        title={'Meet Tokyo'}
-        text={'Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky'}
-      />
-
-      <Container
-        img={shiba}
-        title={'Meet Tokyo'}
-        text={'Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky'}
-      />
-
-<Container
-        img={shiba}
-        title={'Meet Tokyo'}
-        text={'Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky'}
-      />
-
-<Container
-        img={shiba}
-        title={'Meet Tokyo'}
-        text={'Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky'}
-      />
-</div>
     </div>
   );
 }
