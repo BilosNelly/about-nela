f89daefbd33f48c1c8f08e3223f1db8a8ce96fe0
BilosNelly <bilosantonela1996@gmail.com>
2022-11-22 16:41:54 +0100

    Added boxes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import logo from "./logo.svg";
+import shiba from "./shiba.svg";
 import "./App.scss";
 
 function App() {
   return (
     <div className="my-web">
-      <header className="my-web__header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+      <div className="my-web__header">
         <span>Software developer with the crazy shiba inu!</span>
         <span>Meet me, Bilos Antonela</span>
-      </header>
+      </div>
+
+      <div className="my-web__header__img">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div>here will be img</div>
+      </div>
+
+      <div className="my-web__content">
+        <div className="my-web__container__wrapper">
+          <div className="my-web__container">
+            <span className="my-web__container__img">
+              <img src={shiba} className="" alt="shiba-inu" />
+            </span>
+            <span className="my-web__container__title">Meet Tokyo</span>
+            <span className="my-web__container__text">Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky</span>
+          </div>
+        </div>
+
+        <div className="my-web__container__wrapper">
+          <div className="my-web__container">
+            <span className="my-web__container__img">
+              <img src={shiba} className="" alt="shiba-inu" />
+            </span>
+            <span className="my-web__container__title">Meet Tokyo</span>
+            <span className="my-web__container__text">Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky</span>
+          </div>
+        </div>
+
+        <div className="my-web__container__wrapper">
+          <div className="my-web__container">
+            <span className="my-web__container__img">
+              <img src={shiba} className="" alt="shiba-inu" />
+            </span>
+            <span className="my-web__container__title">Meet Tokyo</span>
+            <span className="my-web__container__text">Here will be text about Tokyo Here will be text about Tokyo Here will be text about Toky Here will be text about Toky Here will be text about Toky</span>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
